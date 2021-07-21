41b1611892fc72bf8973888fcde0cdafe586c55f
Somasekhar M <s.sekharofficial@gmail.com>
2021-07-21 15:11:25 +0530

    Implemented bool IsGameOver() and the algorithm to see when is the GameOver and retun bool value

--- TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe.cpp
@@ -1,9 +1,33 @@
 #include<iostream>
 using namespace std;
 
+// checking when the game is over
+bool IsGameOver(char *arr){
+	if(arr[0] == arr[1] == arr[2])
+		return true;
+	else if(arr[3] == arr[4] == arr[5])
+		return true;
+	else if(arr[6] == arr[7] == arr[8])
+		return true;
+	else if(arr[0] == arr[3] == arr[6])
+		return true;
+	else if(arr[1] == arr[4] == arr[7])
+		return true;
+	else if(arr[2] == arr[5] == arr[8])
+		return true;
+	else if(arr[6] == arr[4] == arr[2])
+		return true;
+	else if(arr[0] == arr[4] == arr[8])
+		return true;
+	else
+		return false;
+}
+
+
+
 int main(){
 
-	char arr[9] = {' ',' ',' ',' ',' ',' ',' ',' ',' '};
+	char arr[9] = {'~','!','@','#','$','%','^','&','*'};
 	char p1_choosed, p2_choosed;
 	cout<<"Hello player 1 choose between 'X' and 'O' : ";
 	cin>>p1_choosed;
@@ -21,22 +45,23 @@ int main(){
 
 	cout<<"Awesome ! \n P1 : "<<p1_choosed<<"\n P2 : "<<p2_choosed<<"\n Lets Start ! "<<endl;
 	cout<<"     |     |     "<<endl;
-    cout<<"  "<<arr[0]<<"  |  "<<arr[1]<<"  |  "<<arr[2]<<"  "<<endl;
+    cout<<"     |     |     "<<endl;
     cout<<"_____|_____|_____"<<endl;
     cout<<"     |     |     "<<endl;
-    cout<<"  "<<arr[3]<<"  |  "<<arr[4]<<"  |  "<<arr[5]<<"  "<<endl;
+    cout<<"     |     |     "<<endl;
     cout<<"_____|_____|_____"<<endl;
     cout<<"     |     |     "<<endl;
-    cout<<"  "<<arr[6]<<"  |  "<<arr[7]<<"  |  "<<arr[8]<<"  "<<endl;
     cout<<"     |     |     "<<endl;
+    cout<<"     |     |     "<<endl;
+
+
 
 	return 0;
 }
 
 
 
-char arr[9] = {'X','O','X'};
-    cout<<"     |     |     "<<endl;
+	cout<<"     |     |     "<<endl;
     cout<<"  "<<arr[0]<<"  |  "<<arr[1]<<"  |  "<<arr[2]<<"  "<<endl;
     cout<<"_____|_____|_____"<<endl;
     cout<<"     |     |     "<<endl;
