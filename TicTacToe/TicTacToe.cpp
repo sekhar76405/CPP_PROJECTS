46578f9cedf4cb8346fea9cd10cc5027462ad31e
Somasekhar M <s.sekharofficial@gmail.com>
2021-07-21 15:40:27 +0530

    Corrected the logic of IsGameOver() function

--- TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe.cpp
@@ -3,21 +3,21 @@ using namespace std;
 
 // checking when the game is over
 bool IsGameOver(char *arr){
-	if(arr[0] == arr[1] == arr[2])
+	if(arr[0] == arr[1] && arr[1] == arr[2])
 		return true;
-	else if(arr[3] == arr[4] == arr[5])
+	else if(arr[3] == arr[4] && arr[4]  == arr[5])
 		return true;
-	else if(arr[6] == arr[7] == arr[8])
+	else if(arr[6] == arr[7] && arr[7] == arr[8])
 		return true;
-	else if(arr[0] == arr[3] == arr[6])
+	else if(arr[0] == arr[3] && arr[3] == arr[6])
 		return true;
-	else if(arr[1] == arr[4] == arr[7])
+	else if(arr[1] == arr[4] && arr[4] == arr[7])
 		return true;
-	else if(arr[2] == arr[5] == arr[8])
+	else if(arr[2] == arr[5] && arr[5] == arr[8])
 		return true;
-	else if(arr[6] == arr[4] == arr[2])
+	else if(arr[6] == arr[4] && arr[4] == arr[2])
 		return true;
-	else if(arr[0] == arr[4] == arr[8])
+	else if(arr[0] == arr[4] && arr[4] == arr[8])
 		return true;
 	else
 		return false;
